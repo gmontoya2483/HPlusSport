fa611b4d0217a4dea79c09c705d2a6ede89bbf65
Gabriel Montoya <gabi.montoya@gmail.com>
2021-06-03 09:34:25 -0300

    Add project files.

---
+++ HplusSport.Web/wwwroot/js/api.js
@@ -0,0 +1,23 @@
+﻿var url = "https://localhost:44329/products";
+
+var productList = document.getElementById("products-list");
+if (productList) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => showProducts(data))
+        .catch(ex => {
+            alert("Something went wrong...");
+            console.log(ex);
+        });
+}
+
+
+function showProducts(products) {
+    products.forEach(product => {
+        let li = document.createElement("li");
+        let text = `${product.name} ($ ${product.price})`;
+        li.appendChild(document.createTextNode(text))
+        productList.appendChild(li);
+    });
+
+}
